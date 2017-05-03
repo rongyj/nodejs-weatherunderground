c9adde7482850cb05cf909bcee02b4bca42c3b28
Yongjun Rong <yongjun.rong@oracle.com>
2017-05-03 19:01:19 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#  Display WeatherUnderground PWS weather data in a Node.js site by Using WeatherUnderground API 
+Problem: The purpose of this exercise is to build a prototype that shows we can integrate The Weather Underground into our exisIng NodeJS site. Please display on a site the relevant weather data from a Personal Weather StaIon (PWS).
+
+# Solutions:
+## Solution 1: Client central Solution
+![Client Central Solution](Arch-Diagrams/Slide.jpg)
---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+var $ = require('jquery');
+var fs=require('fs');
+var jsdom = require('jsdom');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+
+app.get('/resume',function(req,res){
+    res.set('content-type','text/html');
+    res.send(fs.readFileSync(__dirname+'/resume.html','utf8'));
+     res.end();
+});
+
+app.get('/', function (req, res) {
+	//$.getJSON('https://api.ipify.org?format=json', function(data){
+	    //console.log(data.ip);
+		//res.send(data.ip);
+		//});
+  res.send('Hellow world !\n');
+});
+
+app.listen(PORT,'0.0.0.0');
+console.log('Running on http://localhost:' + PORT);
